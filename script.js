870fb965ccb857f23b6cc3337cda296c6f2508cb
Diego-Sanchez-88 <enano_sz@hotmail.com>
2024-02-21 17:07:34 +0100

    Se quitan las rutas absolutas

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var images = [
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/cedric-dhaenens-nd4vs7NGXIY-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/florian-olivo-pjyovKCKhSA-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/javier-garcia-J3sivicMj8Y-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/paul-volkmer-1JbTrPMcJ0c-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/peter-herrmann-bhF8gu9DaU4-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/peter-herrmann-eMSlQw-u0ec-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/peter-herrmann-eZaEWy2rAIc-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/peter-herrmann-k9tjyC1D7BY-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/peter-herrmann-sVTloEO5bq4-unsplash.jpg',
-    'C:/Users/enano/OneDrive/Escritorio/ciberseguridad/ThePower/practicas/pinterart/images/radek-kilijanek-tQ6ZYE3nUmM-unsplash.jpg'
+    '/images/cedric-dhaenens-nd4vs7NGXIY-unsplash.jpg',
+    '/images/florian-olivo-pjyovKCKhSA-unsplash.jpg',
+    '/images/javier-garcia-J3sivicMj8Y-unsplash.jpg',
+    '/images/paul-volkmer-1JbTrPMcJ0c-unsplash.jpg',
+    '/images/peter-herrmann-bhF8gu9DaU4-unsplash.jpg',
+    '/images/peter-herrmann-eMSlQw-u0ec-unsplash.jpg',
+    '/images/peter-herrmann-eZaEWy2rAIc-unsplash.jpg',
+    '/images/peter-herrmann-k9tjyC1D7BY-unsplash.jpg',
+    '/images/peter-herrmann-sVTloEO5bq4-unsplash.jpg',
+    '/images/radek-kilijanek-tQ6ZYE3nUmM-unsplash.jpg'
 ];
 
 var galleryContainer = document.getElementById('gallery');
