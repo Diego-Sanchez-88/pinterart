cccd6c6bc609c4716eaa03985614871b2025c3a2
Diego-Sanchez-88 <enano_sz@hotmail.com>
2024-02-21 17:40:23 +0100

    Cambio de fuente y se dan estilos a los botones

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var images = [
-    '/images/cedric-dhaenens-nd4vs7NGXIY-unsplash.jpg',
-    '/images/florian-olivo-pjyovKCKhSA-unsplash.jpg',
-    '/images/javier-garcia-J3sivicMj8Y-unsplash.jpg',
-    '/images/paul-volkmer-1JbTrPMcJ0c-unsplash.jpg',
-    '/images/peter-herrmann-bhF8gu9DaU4-unsplash.jpg',
-    '/images/peter-herrmann-eMSlQw-u0ec-unsplash.jpg',
-    '/images/peter-herrmann-eZaEWy2rAIc-unsplash.jpg',
-    '/images/peter-herrmann-k9tjyC1D7BY-unsplash.jpg',
-    '/images/peter-herrmann-sVTloEO5bq4-unsplash.jpg',
-    '/images/radek-kilijanek-tQ6ZYE3nUmM-unsplash.jpg'
+    'images/cedric-dhaenens-nd4vs7NGXIY-unsplash.jpg',
+    'images/florian-olivo-pjyovKCKhSA-unsplash.jpg',
+    'images/javier-garcia-J3sivicMj8Y-unsplash.jpg',
+    'images/paul-volkmer-1JbTrPMcJ0c-unsplash.jpg',
+    'images/peter-herrmann-bhF8gu9DaU4-unsplash.jpg',
+    'images/peter-herrmann-eMSlQw-u0ec-unsplash.jpg',
+    'images/peter-herrmann-eZaEWy2rAIc-unsplash.jpg',
+    'images/peter-herrmann-k9tjyC1D7BY-unsplash.jpg',
+    'images/peter-herrmann-sVTloEO5bq4-unsplash.jpg',
+    'images/radek-kilijanek-tQ6ZYE3nUmM-unsplash.jpg'
 ];
 
 var galleryContainer = document.getElementById('gallery');
